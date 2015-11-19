cbe0b3eb245aeef96ff9c4839d34a89a272e52bd
ssaini4 <ssaini4@illinois.edu>
2015-11-19 16:02:36 -0600

    done

---
+++ CookieCounter_AppleWatch WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  CookieCounter_AppleWatch WatchKit Extension
+//
+//  Created by Saksham Saini on 7/11/15.
+//  Copyright (c) 2015 Saksham Saini. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ CookieCounter_AppleWatch WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  CookieCounter_AppleWatch WatchKit Extension
+//
+//  Created by Saksham Saini on 7/11/15.
+//  Copyright (c) 2015 Saksham Saini. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
